d8dddae59689a8fcac41d7b61b6b61e18679e7db
Fred <fred@solydxk>
2016-08-19 14:58:51 +0200

    send changeset as parameter of submit function

--- addon/components/bs-form.js
+++ addon/components/bs-form.js
@@ -4,9 +4,27 @@ import BsForm from 'ember-bootstrap/components/bs-form';
 const { computed, RSVP, on, observer } = Ember;
 
 export default BsForm.extend({
-  
-  hasValidator: computed.notEmpty('model.validate'),
 
+  hasValidator: computed.notEmpty('model.validate'),
+  submit(e) {
+      if (e) {
+        e.preventDefault();
+      }
+
+      this.sendAction('before');
+
+      if (!this.get('hasValidator')) {
+        return this.sendAction();
+      } else {
+        let validationPromise = this.validate(this.get('model'));
+        if (validationPromise && validationPromise instanceof Ember.RSVP.Promise) {
+          validationPromise.then((r) => this.sendAction('action', this.get('model'), r), (err) => {
+            this.get('childFormElements').setEach('showValidation', true);
+            return this.sendAction('invalid', this.get('model'), err);
+          });
+        }
+      }
+    },
   validate(model) {
     let m = model;
 
